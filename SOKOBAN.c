51d1ee47ce766742257618674dd07cd63839c979
Julien JACQUET <SolowinFXVI@users.noreply.github.com>
2016-12-10 15:20:51 +0100

    Update SOKOBAN.c

--- SOKOBAN.c
+++ SOKOBAN.c
@@ -8,6 +8,11 @@ return 1;
 int main(int argc, char *argv[]){
 	
 	while(!EXIT()){
+		
+		Lire_fichier(/* fichier*/);
+		Interface_de_jeu(/**/);
+		Interface_de_creation(/**/);
+		Ligne_de_commande(/**/);
 	
 	
 }
