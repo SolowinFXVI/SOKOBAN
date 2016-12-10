b8f30eaccb375fe42f106bc941c5585ce9602a8d
Julien JACQUET <SolowinFXVI@users.noreply.github.com>
2016-12-10 15:39:04 +0100

    Create SOKOBAN.h

---
+++ SOKOBAN.h
@@ -0,0 +1,19 @@
+#include "constantes.h"
+
+#ifndef ___SOKOBAN_H
+#define ___SOKOBAN_H
+
+struct case{
+   int val;
+  };
+  
+typedef struct case CASE;
+  
+struct sokoban{
+  int mode;
+  CASE plateau[largeur_niveau()][hauteur_niveau()];
+  };
+  
+ typedef struct sokoban SOKOBAN;
+ 
+ #endif
